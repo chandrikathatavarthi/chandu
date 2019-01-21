b895628c79e028095263134aa8b03457525980fe
SunilKumar <suniltata33@yahoo.co.in>
2019-01-21 10:25:14 +0530

    test

--- TestProject/src/main/java/com/test/TestProject/Application.java
+++ TestProject/src/main/java/com/test/TestProject/Application.java
@@ -1,20 +1,15 @@
 package com.test.TestProject;
 
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.client.RestTemplate;
-
-import com.test.TestProject.beans.ProductService;
-
 
 	@SpringBootApplication 
 	public class Application {
+
 	   public static void main(String[] args)
 	   {
-	      SpringApplication.run(Application.class, args);
+		   SpringApplication.run(Application.class, args);
 	   }
 	}
 	
