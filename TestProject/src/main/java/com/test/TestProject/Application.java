f6a749e8df50100ec30cb735da51853642a28263
SunilKumar <suniltata33@yahoo.co.in>
2019-01-21 09:30:48 +0530

    Test

--- TestProject/src/main/java/com/test/TestProject/Application.java
+++ TestProject/src/main/java/com/test/TestProject/Application.java
@@ -3,25 +3,19 @@ package com.test.TestProject;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.client.RestTemplate;
 
 import com.test.TestProject.beans.ProductService;
 
 
-public class Application{
-
-	private static final Logger log = LoggerFactory.getLogger(Application.class);
-
-	public static void main(String[] args) {
-		RestTemplate restTemplate = new RestTemplate();
-		ProductService productService = restTemplate.getForObject("https://jl-nonprod-syst.apigee.net/v1/categories/600001506/products?key=2ALHCAAs6ikGRBoy6eTHA58RaG097Fma",ProductService.class);
-		System.out.println(productService.toString());
-
-		
-
-
-
+	@SpringBootApplication 
+	public class Application {
+	   public static void main(String[] args)
+	   {
+	      SpringApplication.run(Application.class, args);
+	   }
 	}
-
-}
+	
 
